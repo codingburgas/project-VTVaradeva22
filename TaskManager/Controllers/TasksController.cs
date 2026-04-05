169039283644ee675b60e8a631c5f1d684c8ee38
Veselina <VTVaradeva22@codingburgas.bg>
2026-04-05 17:21:51 +0300

    Add TaskController and basic task creation

--- TaskManager/Controllers/TasksController.cs
+++ TaskManager/Controllers/TasksController.cs
@@ -1,6 +1,41 @@
-﻿namespace TaskManager.Controllers;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TaskManager.Data.Context;
+using TaskManager.Models.Entities;
 
-public class TasksController
+namespace TaskManager.Controllers
 {
-    
+    public class TaskController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public TaskController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var tasks = await _context.Tasks.Include(t => t.Board).ToListAsync();
+            return View(tasks);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(TaskItem task)
+        {
+            if (!ModelState.IsValid)
+                return View(task);
+
+            _context.Tasks.Add(task);
+            await _context.SaveChangesAsync();
+
+            return RedirectToAction(nameof(Index));
+        }
+    }
 }
+
